86bf793d05dcc52a5fde5e17530c030c7b92c70e
mike <adasd@dd.ru>
2020-07-25 22:28:00 +0200

    - WorkManager переведен на Hilt

--- app/src/main/java/com/lamp/planner/AndroidApp.kt
+++ app/src/main/java/com/lamp/planner/AndroidApp.kt
@@ -1,14 +1,20 @@
 package com.lamp.planner
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.lamp.planner.presentation.background.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class AndroidApp : Application(), Configuration.Provider {
-    //   @Inject lateinit var notificationHelper: NotificationHelper
-//    @Inject lateinit var workerFactory: HiltWorkerFactory
+    @Inject
+    lateinit var notificationHelper: NotificationHelper
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
@@ -17,15 +23,14 @@ class AndroidApp : Application(), Configuration.Provider {
         }
 
         initNotification()
-//        initWorkManager()
     }
 
     private fun initNotification() {
-        //    notificationHelper.createChannels()
+        notificationHelper.createChannels()
     }
 
     override fun getWorkManagerConfiguration() =
         Configuration.Builder()
-//            .setWorkerFactory(workerFactory)
+            .setWorkerFactory(workerFactory)
             .build()
 }
--- app/src/main/java/com/lamp/planner/presentation/background/alarm/BaseAlarmReceiver.kt
+++ app/src/main/java/com/lamp/planner/presentation/background/alarm/BaseAlarmReceiver.kt
@@ -1,11 +1,13 @@
 package com.lamp.planner.presentation.background.alarm
 
-//import com.lamp.planner.presentation.background.workmanager.SendNotifyWorker
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import androidx.work.workDataOf
 import com.lamp.planner.domain.Constants
+import com.lamp.planner.presentation.background.workmanager.SendNotifyWorker
 import timber.log.Timber
 import java.util.*
 
@@ -17,10 +19,10 @@ class BaseAlarmReceiver : BroadcastReceiver() {
         }
         val data = workDataOf(Constants.EVENT_ID to eventId)
 
-//        val worker = OneTimeWorkRequestBuilder<SendNotifyWorker>()
-//            .setInputData(data)
-//            .build()
-//        WorkManager.getInstance(context).enqueue(worker)
+        val worker = OneTimeWorkRequestBuilder<SendNotifyWorker>()
+            .setInputData(data)
+            .build()
+        WorkManager.getInstance(context).enqueue(worker)
 
         Timber.tag("test111").e(Date().toString())
     }
